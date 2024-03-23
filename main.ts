ff0cd743e6b451ed7528c81d34800cadb2179e94
Jeff Borghoff <55985071+jeff-borghoff@users.noreply.github.com>
2024-03-23 11:20:15 -0400

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,20 @@
+input.onButtonPressed(Button.A, function () {
+	
+})
+input.onSound(DetectedSound.Loud, function () {
+    music.play(music.builtinPlayableSoundEffect(soundExpression.sad), music.PlaybackMode.UntilDone)
+    datalogger.log(datalogger.createCV("Time", input.soundLevel()))
+})
+input.onButtonPressed(Button.B, function () {
+	
+})
+input.onGesture(Gesture.Shake, function () {
+	
+})
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+	
+})
+input.setSoundThreshold(SoundThreshold.Loud, 80)
 basic.forever(function () {
 	
 })

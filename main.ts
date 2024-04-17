f7fb388577f42d755194732ca341a8b210ed2e76
Jeff Borghoff <55985071+jeff-borghoff@users.noreply.github.com>
2024-04-17 15:44:59 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,13 @@
+input.onGesture(Gesture.EightG, function () {
+	
+})
 input.onButtonPressed(Button.A, function () {
 	
 })
 input.onGesture(Gesture.FreeFall, function () {
+	
+})
+input.onGesture(Gesture.SixG, function () {
 	
 })
 input.onSound(DetectedSound.Loud, function () {
@@ -17,8 +23,11 @@ input.onGesture(Gesture.ThreeG, function () {
 	
 })
 input.setSoundThreshold(SoundThreshold.Loud, 80)
+input.setAccelerometerRange(AcceleratorRange.OneG)
 basic.forever(function () {
     if (input.acceleration(Dimension.Strength) > 0) {
-    	
+        if (input.isGesture(Gesture.FreeFall)) {
+        	
+        }
     }
 })

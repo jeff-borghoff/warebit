6a3763c43918cb7701708651cd002986f130c1f5
Jeff Borghoff <55985071+jeff-borghoff@users.noreply.github.com>
2024-04-17 12:27:47 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -18,16 +18,16 @@ input.setSoundThreshold(SoundThreshold.Loud, 80)
 basic.forever(function () {
     if (input.acceleration(Dimension.Strength) > 0) {
         if (input.isGesture(Gesture.FreeFall)) {
-        	
+            basic.showNumber(0)
         }
         if (input.isGesture(Gesture.ThreeG)) {
-        	
+            basic.showNumber(1)
         }
         if (input.isGesture(Gesture.SixG)) {
-        	
+            basic.showNumber(2)
         }
         if (input.isGesture(Gesture.EightG)) {
-        	
+            basic.showNumber(3)
         }
     }
 })

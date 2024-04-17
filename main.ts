ec8177c4fc11b52c674640aa4ad8fc6f0299608c
Jeff Borghoff <55985071+jeff-borghoff@users.noreply.github.com>
2024-04-17 12:15:54 -0400

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,6 @@
 input.onButtonPressed(Button.A, function () {
 	
 })
-input.onGesture(Gesture.FreeFall, function () {
-	
-})
 input.onSound(DetectedSound.Loud, function () {
     music.play(music.builtinPlayableSoundEffect(soundExpression.sad), music.PlaybackMode.UntilDone)
     datalogger.log(datalogger.createCV("Time", input.soundLevel()))
@@ -11,23 +8,13 @@ input.onSound(DetectedSound.Loud, function () {
 input.onButtonPressed(Button.B, function () {
 	
 })
+input.onGesture(Gesture.Shake, function () {
+	
+})
 input.onLogoEvent(TouchButtonEvent.Pressed, function () {
 	
 })
 input.setSoundThreshold(SoundThreshold.Loud, 80)
 basic.forever(function () {
-    if (input.acceleration(Dimension.Strength) > 0) {
-        if (input.isGesture(Gesture.FreeFall)) {
-        	
-        }
-        if (input.isGesture(Gesture.ThreeG)) {
-        	
-        }
-        if (input.isGesture(Gesture.SixG)) {
-        	
-        }
-        if (input.isGesture(Gesture.EightG)) {
-        	
-        }
-    }
+    basic.showNumber(input.acceleration(Dimension.Strength))
 })
